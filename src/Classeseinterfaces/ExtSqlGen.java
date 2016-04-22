3e963a4f66d615932f3642e2d75ac6768ca84680
Edson <edsonrcb@hotmail.com>
2016-04-22 20:04:39 -0300

    Classe Interface ExtSqlGen

--- src/Classeseinterfaces/ExtSqlGen.java
+++ src/Classeseinterfaces/ExtSqlGen.java
@@ -1,6 +1,51 @@
 package Classeseinterfaces;
 
-public class ExtSqlGen {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class ExtSqlGen extends SqlGen{
+
+	@Override
+	protected String getCreateTable(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getDropTable(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlInsert(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlSelectAll(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlSelectById(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlUpdateById(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlDeleteById(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	
 	
