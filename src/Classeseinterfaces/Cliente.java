1eef302472cf06be4a1ceaf710d461770057a834
Edson <edsonrcb@hotmail.com>
2016-04-22 20:31:10 -0300

    Classe Cliente pronta

--- src/Classeseinterfaces/Cliente.java
+++ src/Classeseinterfaces/Cliente.java
@@ -15,11 +15,56 @@ public class Cliente {
 	
 	private String telefone;
 	
-	private String 
-	public Cliente(int id, String nome){
+	private Enum estadocivil;
+	
+//	Construtor do metodo cliente
+	
+	public Cliente(int id, String nome, String endereco, String telefone, Enum estadocivil){
 		super();
 		this.id = id;
 		this.nome = nome;
-		
+		this.endereco = endereco;
+		this.telefone = telefone;
+		this.estadocivil = estadocivil;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Enum getEstadocivil() {
+		return estadocivil;
+	}
+
+	public void setEstadocivil(Enum estadocivil) {
+		this.estadocivil = estadocivil;
 	}
 }
