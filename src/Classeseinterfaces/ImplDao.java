f520c8cdb259c470f8c8ce6fa06d8f45981f14d6
Edson <edsonrcb@hotmail.com>
2016-04-22 20:18:36 -0300

    Classe Implements

---
+++ src/Classeseinterfaces/ImplDao.java
@@ -0,0 +1,39 @@
+package Classeseinterfaces;
+
+import java.util.List;
+
+import Classesgenericas.Dao;
+
+public class ImplDao implements Dao<Cliente, Integer>{
+
+	@Override
+	public void salvar(Cliente t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Cliente buscar(Integer k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void atualizar(Cliente t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void excluir(Integer k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Cliente> listarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
